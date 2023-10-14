9bdffdbbd97e2a7195784fb439d5dc663f624ec1
Quark <146047521+JackQuark@users.noreply.github.com>
2023-10-14 17:27:50 +0800

    Update midd.py

--- 107/midd.py
+++ 107/midd.py
@@ -4,14 +4,14 @@ import midd_f            as f
 
 Ps, H, Ts, RHs = np.loadtxt('midc_input.txt', dtype=float, comments=None, skiprows=1, unpack='true')
 
-P  = np.delete( Ps, np.where(Ps < 700))
-RH = np.delete(RHs, np.argwhere(Ps < 700))
-CF = f.cal_cf (P, RH)
+P   = np.delete( Ps, np.where(Ps < 700))
+RH  = np.delete(RHs, np.argwhere(Ps < 700))
+CFs = f.cal_cf (P, RH)
 
-P20  = np.delete( P, np.argwhere(CF <= 20))
-CF20 = np.delete(CF, np.argwhere(CF <= 20))
+P20  = np.delete(  P, np.argwhere(CFs <= 20))
+CF20 = np.delete(CFs, np.argwhere(CFs <= 20))
 
-plt.plot(CF, P, 'g-.')
+plt.plot(CFs, P, 'g-.')
 plt.plot(CF20, P20, 'gd')
 plt.plot(CF20[len(CF20)-1], min(P20), 'rd')
 plt.xlim(5   , 40)
