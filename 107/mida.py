cb1e41909cb39459bda7460659940352d78e2832
Quark <146047521+JackQuark@users.noreply.github.com>
2023-10-15 10:24:29 +0800

    Update mida.py

--- 107/mida.py
+++ 107/mida.py
@@ -1,14 +1,10 @@
 import numpy             as np
 import math              as m
 
-n, Sn  = float(input('n  :  ')), 1
-if(n-int(n) < 0.5):
-    n = int(n)
-else:
-    n = int(n)+1
+n, Sn  = round(input('n  :  ')), 1
 
 for i in range(n):
     Sn += 1/m.factorial(i+1)
     
-print('Sn = ', Sn)
-print('e  = ',m.e)
+print('Sn = ',  Sn)
+print('e  = ', m.e)
