a8549ac934af73e1972010c0b5fdbea5da96f19c
Quark <146047521+JackQuark@users.noreply.github.com>
2023-10-16 12:06:23 +0800

    Update text_xmas.py

--- 110/text_xmas.py
+++ 110/text_xmas.py
@@ -1,10 +1,10 @@
-nb, nt, t = 4, 2, 0
+nt, nb, t = 4, 2, 0
 
 for i in range(nb+nt):
-    if(i < nb):
-        print('0'* (nb-1-i) + '1'* (2*i+1) + '0'* (nb-1-i))
+    if(i < nt):
+        print('0'* (nt-1-i) + '1'* (2*i+1) + '0'* (nt-1-i))
         
-        if(i == nb-3):t = i
+        if(i == nt-3):t = i
         
     else:
-        print('0'* (nb-1-t) + '1'* (2*t+1) + '0'* (nb-1-t))
+        print('0'* (nt-1-t) + '1'* (2*t+1) + '0'* (nt-1-t))
